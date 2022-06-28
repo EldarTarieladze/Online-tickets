f7860e0d6fc3f37aa89d92a7ed14bdf2e0269e05
EldarTarieladze <68877428+EldarTarieladze@users.noreply.github.com>
2022-06-28 21:18:31 +0400

    fix: sequelize options and change postgresql connection

--- src/core/database/database.providers.ts
+++ src/core/database/database.providers.ts
@@ -6,34 +6,34 @@ import { Ticket } from 'src/models/ticket.model';
 import { SEQUELIZE, DEVELOPMENT, TEST, PRODUCTION } from '../constants';
 import { databaseConfig } from './database.config';
 
-export const databaseProviders = [{
-   provide: SEQUELIZE,
-   useFactory: async () => {
+export const databaseProviders = [
+  {
+    provide: SEQUELIZE,
+    useFactory: async () => {
       let config;
       switch (process.env.NODE_ENV) {
-         case DEVELOPMENT:
-            config = databaseConfig.development;
-            break;
-         case TEST:
-            config = databaseConfig.test;
-            break;
-         case PRODUCTION:
-            config = databaseConfig.production;
-            break;
-         default:
-            config = databaseConfig.development;
+        case DEVELOPMENT:
+          config = databaseConfig.development;
+          break;
+        case TEST:
+          config = databaseConfig.test;
+          break;
+        case PRODUCTION:
+          config = databaseConfig.production;
+          break;
+        default:
+          config = databaseConfig.development;
       }
       const sequelize = new Sequelize(config);
       sequelize.addModels([Ticket, Events, Reservation]);
-      if(process.env.autoSyncDb === 'true'){
-         await sequelize.sync({ force: true }).then(() => {
-            exec(
-               'npx sequelize db:seed:all',(err) => {
-                  if(err) console.error(err)
-               }
-            )
-         });
+      if (process.env.autoSyncDb === 'false') {
+        await sequelize.sync({ force: true }).then(() => {
+          exec('npx sequelize db:seed:all', (err) => {
+            if (err) console.error(err);
+          });
+        });
       }
       return sequelize;
-   },
-}];
+    },
+  },
+];
