1979c82c4bfec4be8c753404d5e70649655e3b59
EldarTarieladze <68877428+EldarTarieladze@users.noreply.github.com>
2022-06-29 20:20:30 +0400

    feat: change reservation dto

--- seeders/20220626172211-events.js
+++ seeders/20220626172211-events.js
@@ -3,46 +3,57 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     try {
-      await queryInterface.bulkInsert('events', [{
-        eventName: 'Event - 1',
-        description: 'Event - 1 description',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }, {
-        eventName: 'Event - 2',
-        description: 'Event - 2 description',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }, {
-        eventName: 'Event - 3',
-        description: 'Event - 3 description',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }, {
-        eventName: 'Event - 4',
-        description: 'Event - 4 description',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }])
+      await queryInterface.bulkInsert('events', [
+        {
+          eventName: 'Event - 1',
+          description: 'Event - 1 description',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        // {
+        //   eventName: 'Event - 2',
+        //   description: 'Event - 2 description',
+        //   createdAt: new Date(),
+        //   updatedAt: new Date(),
+        // },
+        // {
+        //   eventName: 'Event - 3',
+        //   description: 'Event - 3 description',
+        //   createdAt: new Date(),
+        //   updatedAt: new Date(),
+        // },
+        // {
+        //   eventName: 'Event - 4',
+        //   description: 'Event - 4 description',
+        //   createdAt: new Date(),
+        //   updatedAt: new Date(),
+        // },
+      ]);
       const events = await queryInterface.sequelize.query(
-        `SELECT id from events;`
+        `SELECT id from events;`,
       );
-      let ticket = []
-      events[0].map(event => {
-        for (let k = 0; k < 10; k++) {
+      let ticket = [];
+      const min = 4;
+      const max = 6;
+      const random = (min, max) =>
+        Math.floor(Math.random() * (max - min)) + min;
+      console.log(events);
+      events[0].map((event) => {
+        for (let k = 0; k < 20; k++) {
           ticket.push({
             name: `Ticket - ${k}`,
-            rowNumber: 5,
-            sellingOptions: Sequelize.literal(`ARRAY['EVEN', 'ALL_TOGETHER']::"enum_tickets_sellingOptions"[]`),
+            rowNumber: random(min, max),
+            sellingOptions: Sequelize.literal(
+              `ARRAY['EVEN', 'ALL_TOGETHER']::"enum_tickets_sellingOptions"[]`,
+            ),
             eventId: event.id,
             createdAt: new Date(),
-            updatedAt: new Date()
-          })
+            updatedAt: new Date(),
+          });
         }
-      })
-      console.log(ticket)
-      await queryInterface.bulkInsert('tickets', ticket)
-
+      });
+      console.log(ticket);
+      await queryInterface.bulkInsert('tickets', ticket);
     } catch (err) {
       throw err;
     }
@@ -54,7 +65,7 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
+     */
   },
 
   async down(queryInterface, Sequelize) {
@@ -62,5 +73,5 @@ module.exports = {
     // await queryInterface.dropEnum('enum_reservations_paymentStatus')
     // await queryInterface.dropEnum('enum_tickets_sellingOptions')
     await queryInterface.bulkDelete('events', null, {});
-  }
+  },
 };
--- src/modules/reservation/reservation.service.ts
+++ src/modules/reservation/reservation.service.ts
@@ -1,81 +1,122 @@
-import { HttpException, HttpStatus, Inject, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectConnection, InjectModel } from '@nestjs/sequelize';
 import { Sequelize } from 'sequelize-typescript';
 import { Transaction } from 'sequelize/types';
-import { RESERVATION_REPOSITORY, SEQUELIZE, TICKET_REPOSITORY } from 'src/core/constants';
+import {
+  RESERVATION_REPOSITORY,
+  SEQUELIZE,
+  TICKET_REPOSITORY,
+} from 'src/core/constants';
 import { Reservation } from 'src/models/reservation.model';
 import { Ticket } from 'src/models/ticket.model';
 import { CreateReservationDto } from './dto/reservation.dto';
 
 export enum sellingOptions {
-    AVOID_ONE = 'AVOID_ONE',
-    ALL_TOGETHER = 'ALL_TOGETHER',
-    EVEN = 'EVEN'
+  AVOID_ONE = 'AVOID_ONE',
+  ALL_TOGETHER = 'ALL_TOGETHER',
+  EVEN = 'EVEN',
+}
+
+function groupBy(arr: Ticket[], prop: string) {
+  const map = new Map(Array.from(arr, (obj) => [obj[prop], []]));
+  arr.forEach((obj) => map.get(obj[prop]).push(obj));
+  return Array.from(map.values());
 }
 
 @Injectable()
 export class ReservationService {
-    constructor(
-        @Inject(RESERVATION_REPOSITORY) private readonly reservationReopsitory: typeof Reservation,
-        @Inject(TICKET_REPOSITORY) private readonly ticketRepository: typeof Ticket,
-        @Inject(SEQUELIZE) private sequelize: Sequelize
-    ) { }
+  constructor(
+    @Inject(RESERVATION_REPOSITORY)
+    private readonly reservationReopsitory: typeof Reservation,
+    @Inject(TICKET_REPOSITORY) private readonly ticketRepository: typeof Ticket,
+    @Inject(SEQUELIZE) private sequelize: Sequelize,
+  ) {}
 
-    async createReservation(dto: CreateReservationDto) {
-        const trans = await this.sequelize.transaction()
-        try {
-            const lockTickets: Ticket[] = []
-            for await (const tId of dto.ticketIds) {
-                const ticket = await this.ticketRepository.findByPk<Ticket>(tId, { lock: true, skipLocked: true, transaction: trans })
-                if (!ticket) {
-                    console.log(1)
-                    throw new HttpException({ message: `ticket with the Id ${tId} is not available!` }, HttpStatus.NOT_FOUND)
-                }
-
-                if (ticket.reservationId) {
-                    throw new HttpException({ message: `Ticket with Id ${tId} is already reserved!` }, HttpStatus.FORBIDDEN)
-                }
-
-                lockTickets.push(ticket)
-            }
-
-            for await (const ticket of lockTickets) {
-                for await (const option of ticket.sellingOptions) {
-                    switch (option) {
-                        case sellingOptions.EVEN:
-                            if (lockTickets.length % 2 !== 0) throw new HttpException({ message: `Ticket Id ${ticket.id} has EVEN selling option, you have to buy EVEN Tickets` }, HttpStatus.FORBIDDEN)
-                            break;
-
-                        case sellingOptions.AVOID_ONE:
-                            const availableTicket = await this.ticketRepository.findAndCountAll({
-                                where: {
-                                    eventId: ticket.eventId,
-                                    rowNumber: ticket.rowNumber,
-                                    reservationId: null
-                                },
-                                skipLocked: true,
-                                transaction: trans
-                            })
-                            if (availableTicket.count === 1) throw new HttpException({ message: `Ticket Id ${ticket.id} has AVOID_ONE selling option` }, HttpStatus.FORBIDDEN)
-                            break;
-                        case sellingOptions.ALL_TOGETHER:
-                                const sortedTicket = dto.ticketIds.sort()
-                                console.log(sortedTicket[sortedTicket.length-1] - sortedTicket[0] === sortedTicket.length -1)
-                                // if(sortedTicket[sortedTicket.length-1] - sortedTicket[0] === sortedTicket.length -1)
-
-
-                            break
-                        default:
-                            break;
-                    }
-                }
-            }
-            await trans.commit()
-            return lockTickets
-
-        } catch (error) {
-            await trans.rollback()
-            throw error
+  async createReservation(dto: CreateReservationDto) {
+    const trans = await this.sequelize.transaction();
+    try {
+      const lockTickets: Ticket[] = [];
+      let ticketGroupByRowNumber: Array<Ticket[]> = [];
+      for await (const tId of dto.ticketIds) {
+        const ticket = await this.ticketRepository.findByPk<Ticket>(tId, {
+          lock: true,
+          skipLocked: true,
+          transaction: trans,
+        });
+        if (!ticket) {
+          console.log(1);
+          throw new HttpException(
+            { message: `ticket with the Id ${tId} is not available!` },
+            HttpStatus.NOT_FOUND,
+          );
         }
+
+        if (ticket.reservationId) {
+          throw new HttpException(
+            { message: `Ticket with Id ${tId} is already reserved!` },
+            HttpStatus.FORBIDDEN,
+          );
+        }
+
+        lockTickets.push(ticket);
+      }
+      ticketGroupByRowNumber = groupBy(lockTickets, 'rowNumber');
+
+      for await (const ticketArray of ticketGroupByRowNumber) {
+        for await (const ticket of ticketArray) {
+          for await (const option of ticket.sellingOptions) {
+            switch (option) {
+              case sellingOptions.EVEN:
+                if (lockTickets.length % 2 !== 0)
+                  throw new HttpException(
+                    {
+                      message: `Ticket Id ${ticket.id} has EVEN selling option, you have to buy EVEN Tickets`,
+                    },
+                    HttpStatus.FORBIDDEN,
+                  );
+                break;
+
+              case sellingOptions.AVOID_ONE:
+                const availableTicket =
+                  await this.ticketRepository.findAndCountAll({
+                    where: {
+                      eventId: ticket.eventId,
+                      rowNumber: ticket.rowNumber,
+                      reservationId: null,
+                    },
+                    skipLocked: true,
+                    transaction: trans,
+                  });
+                if (availableTicket.count === 1)
+                  throw new HttpException(
+                    {
+                      message: `Ticket Id ${ticket.id} has AVOID_ONE selling option`,
+                    },
+                    HttpStatus.FORBIDDEN,
+                  );
+                break;
+              case sellingOptions.ALL_TOGETHER:
+                /// issue
+                // if(sortedTicket[sortedTicket.length-1] - sortedTicket[0] === sortedTicket.length -1)
+                break;
+              default:
+                break;
+            }
+          }
+        }
+      }
+
+      await trans.commit();
+      return lockTickets;
+    } catch (error) {
+      await trans.rollback();
+      throw error;
     }
+  }
 }
